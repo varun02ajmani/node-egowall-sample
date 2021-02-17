bef235f718fb912021042e7675bd29e723bf3098
Varun Ajmani <varunajmani@gmail.com>
2021-02-17 21:13:52 +0530

    version update

--- app.js
+++ app.js
@@ -2,15 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("Welcome to the EgoWall Test Pipeline Home Page Version 3.0");
-})
-
-app.get('/get', (req,res) => {
-    res.send("Welcome to the EgoWall Test Pipeline GET REQUEST");
-})
-
-app.get('/varun', (req,res) => {
-    res.send("Varun is here");
+    res.send("Welcome to the EgoWall Test Pipeline Home Page Version 4.0");
 })
 
 const port = process.env.port || 3000;
