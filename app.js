71e83f994a15515fe7be51370fb113ef62ad8f60
Varun Ajmani <varunajmani@gmail.com>
2021-02-17 16:20:07 +0530

    added new method

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/get', (req,res) => {
     res.send("Welcome to the EgoWall Test Pipeline GET REQUEST");
 })
 
+app.get('/varun', (req,res) => {
+    res.send("Varun is here");
+})
+
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log("version 2.0");
