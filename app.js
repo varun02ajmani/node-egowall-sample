5e8326df81fb1285ee70da208f704fc4cc8673a6
Varun Ajmani <varunajmani@gmail.com>
2021-02-17 15:42:52 +0530

    this is a get setup

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req,res) => {
     res.send("Welcome to the EgoWall Test Pipeline Home Page Version 3.0");
 })
 
+app.get('/get', (req,res) => {
+    res.send("Welcome to the EgoWall Test Pipeline GET REQUEST");
+})
+
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log("version 2.0");
